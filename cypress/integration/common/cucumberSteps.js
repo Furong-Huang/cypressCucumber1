4d4e0b84ba57c6ee66c85785a77370ddcd0ed014
Furong Huang <furonghuang1314@outlook.com>
2021-02-03 11:26:02 +1300

    initial commit

---
+++ cypress/integration/common/cucumberSteps.js
@@ -0,0 +1,26 @@
+import { should } from 'chai'
+import {Given, When, Then} from 'cypress-cucumber-preprocessor/steps'
+
+Given('I am on the cucumber.js GitHub repository',()=>{
+     cy.visit('https://github.com/cucumber/cucumber-js')
+})
+
+When('I go to the README file',()=>{
+     cy.get('[title="README.md"]').click()
+})
+
+Then('I should see a {string} section',(content)=>{
+     cy.get('article.markdown-body').should('not.contain',content).then(()=>{
+          console.log('This ' + content + " was not found.");
+     })
+})
+
+
+Then('I should see a Build Status and Dependencies badge',()=>{
+     cy.get('[alt="GitHub Actions"]').invoke('attr','src').then((src)=>{
+          expect(src).to.be.exist
+     })
+     cy.get('[alt="Dependencies"]').invoke('attr','src').then((src)=>{
+          expect(src).to.be.exist
+     })
+})
